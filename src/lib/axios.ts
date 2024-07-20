a399e4475b54e754ff71b04fb1160ad88012ddb9
Glaucco <glauccolima@gmail.com>
2024-07-19 22:38:30 -0300

    update

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 // Cria uma instância do Axios configurada para uso em toda a aplicação
 export const api = axios.create({
-  baseURL: "http://localhost:3333", // Define a URL base para todas as chamadas HTTP feitas através desta instância
+  baseURL: "https://turbo-palm-tree-9rgqp5vgrwjc9p9x-3333.app.github.dev/", // Define a URL base para todas as chamadas HTTP feitas através desta instância
 });
