ddf022eef95bc0241ca12d2d6bc8b56450adfb25
Glaucco Siqueira <113634929+glauccoslima@users.noreply.github.com>
2024-08-06 14:48:19 -0300

    Update API Node Vercel

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 // Cria uma instância do Axios configurada para uso em toda a aplicação
 export const api = axios.create({
-  baseURL: "https://node-api-nlw.vercel.app/", // Define a URL base para todas as chamadas HTTP feitas através desta instância
+  baseURL: "https://glauccoslima-node-api.vercel.app/", // Define a URL base para todas as chamadas HTTP feitas através desta instância
 });
